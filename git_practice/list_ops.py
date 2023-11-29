640e5eeeaf0b31aa597f30da80068e0e74043532
seokjinyoo <ysj9800@naver.com>
2023-11-29 14:08:41 +0900

    implement multiply and substract

--- git_practice/list_ops.py
+++ git_practice/list_ops.py
@@ -17,6 +17,18 @@ def subtract(foo, bar):
 	#	dst.append(foo[i] + bar[i])
 	#return dst
 
+def multiply(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f * b)
+    return out
+
+def divide(foo, bar):
+    out = []
+    for f, b in zip(foo, bar):
+        out.append(f / b)
+    return out
+
 if __name__ == "__main__":
 	dst = add_lists([1,2,3], [4,5,6])
 	print(dst)
