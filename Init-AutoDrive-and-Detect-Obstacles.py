64b0d230e56f64938294c82ae7e4b179cbeee7a6
seokjinyoo <ysj9800@naver.com>
2023-12-12 10:25:42 +0900

    add new file

---
+++ Init-AutoDrive-and-Detect-Obstacles.py
@@ -0,0 +1,14 @@
+# Init_AutoDrive_and_Detect_Obstacles.py
+
+class AutoDrive:
+    def __init__(self):
+        # 클래스 초기화 및 레이저 스캔 구독 코드
+        pass
+
+    def laser_callback(self, scan):
+        # 레이저 스캔을 받아 처리하는 코드
+        pass
+
+    def identify_obstacle(self, scan):
+        # 장애물 유형 판별 코드
+        pass
---
+++ Set_Speed_and_Drive_Based_on_Obstacles.py
@@ -0,0 +1,26 @@
+
+class AutoDrive:
+    def set_velocity_based_on_obstacle(self, scan):
+        # 장애물 유형에 따른 속도 설정 코드
+        pass
+
+    def set_velocity_for_obstacle_type_1(self, vel):
+        # 장애물 유형 1에 대한 속도 설정 코드
+        pass
+
+    def set_velocity_for_obstacle_type_2(self, vel):
+        # 장애물 유형 2에 대한 속도 설정 코드
+        pass
+
+    def set_velocity_for_obstacle_type_3(self, vel):
+        # 장애물 유형 3에 대한 속도 설정 코드
+        pass
+
+    def set_velocity_for_obstacle_type_4(self, vel):
+        # 장애물 유형 4에 대한 속도 설정 코드
+        pass
+
+    def set_velocity_for_no_obstacle(self, vel):
+        # 장애물이 없을 때의 속도 설정 코드
+        pass
+
